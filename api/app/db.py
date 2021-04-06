f5e2f70223862417625dbe27f8a2f14b68b7e44c
Purvis Samsoodeen <purviss2@illinois.edu>
2021-04-06 09:30:10 -0500

    added modification to save trips for the api

--- api/app/db.py
+++ api/app/db.py
@@ -36,6 +36,41 @@ def call_sp(sp, params):
 
     return returnRow
 
+def call_fn(fn, params):
+    conn = None
+    returnRow = None
+    try:
+        conn = psycopg2.connect(
+            host = "172.22.152.7",
+            database = "postgres",
+            user = "admin",
+	    password = "admin"
+        )
+        cur  = conn.cursor()
+        converted = []
+        for (x,y) in params:
+           if y == "text":
+             converted.append("'{}'".format(x))
+           elif y == "array":
+             aha = [str(z) for z in x]
+             temp = ",".join(aha)
+             converted.append("'{{{}}}'".format(temp))             
+           else:
+             raise ValueError("param datatype not specified")
+
+        query = "select {0}({1});".format(fn, ",".join(converted))
+        print (query)
+        cur.execute(query)
+        conn.commit()
+        returnRow = cur.fetchall()
+        cur.close()
+    except Exception as ex:
+        print (ex)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return returnRow
 
 def connect(query):
     """ connect to the db"""
